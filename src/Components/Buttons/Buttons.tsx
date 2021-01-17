d7ed8a4e94be2cf36d72383628cae6422e08d875
Ngassaki <cardorelngassaki@gmail.com>
2021-01-17 08:11:27 +0200

    Modoficatin in App.tsx

--- src/Components/Buttons/Buttons.tsx
+++ src/Components/Buttons/Buttons.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Button.style.css";
 
 type Props = {
-  startBtn: any;
-  waitBtn: any;
-  resetBtn: any;
-  stopBtn: any;
+  startBtn?: any;
+  waitBtn?: any;
+  resetBtn?: any;
+  stopBtn?: any;
   isStartCompleted: boolean;
   isDisabled?: boolean | undefined;
 };
