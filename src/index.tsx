ae28fff512c82557d1f5998cc9eb2c81909411fb
Ngassaki <cardorelngassaki@gmail.com>
2021-01-17 07:51:26 +0200

    Added Suspense

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React , {lazy , Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 const Gobal_App = lazy(() => import('./App'));
 
